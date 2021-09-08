3443210efde14d6784cd46f56f90705abe40decf
tucci.lee <tucci.lee@aliyun.com>
2021-09-08 13:30:42 +0800

    FilterChain 修改

--- servlet/src/main/java/cn/tucci/servlet/filter/ChainProxy.java
+++ servlet/src/main/java/cn/tucci/servlet/filter/ChainProxy.java
@@ -17,24 +17,22 @@ public class ChainProxy implements FilterChain {
 
     private FilterChain originalChain;
     private List<Filter> filters;
-    private int size = 0;
     private int index = 0;
 
     public ChainProxy(FilterChain originalChain, List<Filter> filters) {
-        this.originalChain = originalChain;
-        if(filters != null && !filters.isEmpty()){
-            size = filters.size();
-            this.filters = filters;
+        if (originalChain == null) {
+            throw new NullPointerException("original FilterChain cannot be null");
         }
+        this.originalChain = originalChain;
+        this.filters = filters;
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException {
-        if (index == size) {
+        if (filters == null || index == filters.size()) {
             originalChain.doFilter(servletRequest, servletResponse);
-        }else {
-            index++;
-            Filter nextFilter = filters.get(index - 1);
+        } else {
+            Filter nextFilter = filters.get(index++);
             nextFilter.doFilter(servletRequest, servletResponse, this);
         }
     }
